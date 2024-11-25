28752c641d1f021290d75f1f8c2498c1eb5987c3
Mud Man <chickensrule111@gmail.com>
2024-11-26 10:38:57 +1100

    reset due to errors

--- users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Profile
-
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'first_name', 'surname', 'nickname')
-    search_fields = ('user__username', 'nickname')
-    list_filter = ('nickname',)
-    
-admin.site.register(Profile, ProfileAdmin)
--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.user, name="user"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path('register/', views.register, name='register'),
-]
