ae503ffbe2fdb2c00e330d21bd13da40cfc36c78
Mud Man <chickensrule111@gmail.com>
2024-11-26 20:28:56 +1100

    Refactor Invitation Email Notification (Week 4)

--- chipin/models.py
+++ chipin/models.py
@@ -5,6 +5,7 @@ class Group(models.Model):
     name = models.CharField(max_length=100)
     admin = models.ForeignKey(User, related_name='admin_groups', on_delete=models.CASCADE)
     members = models.ManyToManyField(User, related_name='group_memberships', blank=True)
+    invited_users = models.ManyToManyField(User, related_name='pending_invitations', blank=True)
 
     def __str__(self):
         return self.name
